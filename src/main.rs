973104ffb44f8188de9c862b995354f8be63e0b0
Sam Szuflita <sszuflit@caltech.edu>
2019-10-13 22:18:33 -0400

    Simple event loop

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,44 @@
+extern crate piston_window;
+
+use piston_window::*;
+use piston_window::Button::Keyboard;
+use piston_window::Key;
+
 fn main() {
-    println!("Hello, world!");
-}
+    let mut window: PistonWindow =
+        WindowSettings::new("Hello Piston!", [640, 480])
+            .exit_on_esc(true).build().unwrap();
 
-fn add() -> i32 {
-    return 5;
-}
+    let mut x = 0.0;
+    let mut y = 0.0;
 
-fn test() -> String {
-    return String::from("asdf");
-}
+    while let Some(event) = window.next() {
+        window.draw_2d(&event, |context, graphics, _device| {
+            clear([1.0; 4], graphics);
+            rectangle([1.0, 0.0, 0.0, 1.0], // red
+                      [x, y, 100.0, 100.0],
+                      context.transform,
+                      graphics);
 
-#[cfg(test)]
-mod tests {
-    // Note this useful idiom: importing names from outer (for mod tests) scope.
-    use super::*;
 
-    #[test]
-    fn test_add() {
-        assert_eq!(add(), 3);
-    }
+        });
 
-    #[test]
-    fn test_test() {
-        assert_eq!(add(), String::from("asdf"));
+        if let Some(Button::Keyboard(key)) = event.press_args() {
+            match key {
+                Key::Up => {
+                    y -= 10.0;
+                }
+                Key::Down => {
+                    y += 10.0;
+                }
+                Key::Left => {
+                    x -= 10.0;
+                }
+                Key::Right => {
+                    x += 10.0;
+                }
+                _ => {}
+            }
+        }
     }
 }
